383b06ffa5ae786ea3d6bb7e221ee2e95b82adda
LiuYF <liuyf@cssweb.com.cn>
2017-10-25 21:11:51 +0800

    init

---
+++ src/c-sharp.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SSY
+{
+	class ssy
+	{
+		static int[] array = {10, 20, 30, 40, 50};
+
+		static void Main(string [] args)
+		{
+			foreach(int i in array)
+				Console.WriteLine(i);
+		}
+	}
+}
